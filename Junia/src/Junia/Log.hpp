19ba168eb8e2eb68bf5751dc73042ca23e37cac8
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-05-15 00:47:33 +0200

    Fixed Log warning when logging only one string

--- Junia/src/Junia/Log.hpp
+++ Junia/src/Junia/Log.hpp
@@ -6,40 +6,40 @@
 #include "Base.hpp"
 
 #if defined(JELOG_MAX_TRACE)
-	#define JELOG_BASE_TRACE(str, ...) Junia::Log::JuniaLogTrace (__FILE__, __LINE__, str, __VA_ARGS__)
-	#define JELOG_TRACE(str, ...)      Junia::Log::LogTrace      (__FILE__, __LINE__, str, __VA_ARGS__)
+	#define JELOG_BASE_TRACE(str, ...) Junia::Log::JuniaLogTrace (__FILE__, __LINE__, str, ##__VA_ARGS__)
+	#define JELOG_TRACE(str, ...)      Junia::Log::LogTrace      (__FILE__, __LINE__, str, ##__VA_ARGS__)
 #else
 	#define JELOG_BASE_TRACE(...)
 	#define JELOG_TRACE(...)
 #endif
 
 #if defined(JELOG_MAX_TRACE) || defined(JELOG_MAX_INFO)
-	#define JELOG_BASE_INFO(str, ...)  Junia::Log::JuniaLogInfo  (__FILE__, __LINE__, str, __VA_ARGS__)
-	#define JELOG_INFO(str, ...)       Junia::Log::LogInfo       (__FILE__, __LINE__, str, __VA_ARGS__)
+	#define JELOG_BASE_INFO(str, ...)  Junia::Log::JuniaLogInfo  (__FILE__, __LINE__, str, ##__VA_ARGS__)
+	#define JELOG_INFO(str, ...)       Junia::Log::LogInfo       (__FILE__, __LINE__, str, ##__VA_ARGS__)
 #else
 	#define JELOG_BASE_INFO(...)
 	#define JELOG_INFO(...)
 #endif
 
 #if defined(JELOG_MAX_TRACE) || defined(JELOG_MAX_INFO) || defined(JELOG_MAX_WARN)
-	#define JELOG_BASE_WARN(str, ...)  Junia::Log::JuniaLogWarn  (__FILE__, __LINE__, str, __VA_ARGS__)
-	#define JELOG_WARN(str, ...)       Junia::Log::LogWarn       (__FILE__, __LINE__, str, __VA_ARGS__)
+	#define JELOG_BASE_WARN(str, ...)  Junia::Log::JuniaLogWarn  (__FILE__, __LINE__, str, ##__VA_ARGS__)
+	#define JELOG_WARN(str, ...)       Junia::Log::LogWarn       (__FILE__, __LINE__, str, ##__VA_ARGS__)
 #else
 	#define JELOG_BASE_WARN(...)
 	#define JELOG_WARN(...)
 #endif
 
 #if defined(JELOG_MAX_TRACE) || defined(JELOG_MAX_INFO) || defined(JELOG_MAX_WARN) || defined(JELOG_MAX_ERROR)
-	#define JELOG_BASE_ERROR(str, ...) Junia::Log::JuniaLogError (__FILE__, __LINE__, str, __VA_ARGS__)
-	#define JELOG_ERROR(str, ...)      Junia::Log::LogError      (__FILE__, __LINE__, str, __VA_ARGS__)
+	#define JELOG_BASE_ERROR(str, ...) Junia::Log::JuniaLogError (__FILE__, __LINE__, str, ##__VA_ARGS__)
+	#define JELOG_ERROR(str, ...)      Junia::Log::LogError      (__FILE__, __LINE__, str, ##__VA_ARGS__)
 #else
 	#define JELOG_BASE_ERROR(...)
 	#define JELOG_ERROR(...)
 #endif
 
 #if defined(JELOG_MAX_TRACE) || defined(JELOG_MAX_INFO) || defined(JELOG_MAX_WARN) || defined(JELOG_MAX_ERROR) || defined(JELOG_MAX_CRIT)
-	#define JELOG_BASE_CRIT(str, ...)  Junia::Log::JuniaLogCrit  (__FILE__, __LINE__, str, __VA_ARGS__)
-	#define JELOG_CRIT(str, ...)       Junia::Log::LogCrit       (__FILE__, __LINE__, str, __VA_ARGS__)
+	#define JELOG_BASE_CRIT(str, ...)  Junia::Log::JuniaLogCrit  (__FILE__, __LINE__, str, ##__VA_ARGS__)
+	#define JELOG_CRIT(str, ...)       Junia::Log::LogCrit       (__FILE__, __LINE__, str, ##__VA_ARGS__)
 #else
 	#define JELOG_BASE_CRIT(...)
 	#define JELOG_CRIT(...)
