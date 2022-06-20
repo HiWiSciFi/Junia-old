081c271315330a95703c96cc573406fd307f7534
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-06-20 22:54:49 +0200

    reduced testapp

--- Testing/src/Testapp.cpp
+++ Testing/src/Testapp.cpp
@@ -1,34 +1,9 @@
 #include <Junia.hpp>
-#include <Junia/Events/EventSystem.hpp>
-#include <cstdarg>
-
-class ExampleLayer final : public Junia::Layer
-{
-public:
-	ExampleLayer() : Layer("Example Layer") { }
-
-	void OnEnable() override
-	{
-		Junia::EventSystem::Subscribe(JE_EVENT_BIND_MEMBER_FUNC(OnEvent));
-		JELOG_TRACE("Layer name: " JELOG_CSTR, name.c_str());
-	}
-
-	bool OnEvent(const Junia::Event* e) const
-	{
-		if (e->GetCategory() != Junia::EventCategory::Window && e->GetType() != Junia::EventType::MouseMove)
-			JELOG_TRACE("(Layer) " JELOG_CSTR ": " JELOG_CSTR, name.c_str(), e->ToString().c_str());
-		return false;
-	}
-};
 
 class Testapp : public Junia::Application
 {
 public:
-	Testapp()
-	{
-		PushLayerFront(new ExampleLayer());
-	}
-
+	Testapp() { }
 	~Testapp() override = default;
 };
 
