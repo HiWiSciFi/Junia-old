e80891357f8a20b3a5799aa5365ef67fbcdd4ce8
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-06-21 08:29:07 +0200

    added imgui

---
+++ Junia/dependencies/premake-imgui.lua
@@ -0,0 +1,46 @@
+project "imgui"
+    location "%{prj.name}"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "On"
+
+	targetdir ("out/%{prj.name}/" .. buildtargetname)
+	objdir ("out-obj/%{prj.name}/" .. buildtargetname)
+
+	files {
+		"%{prj.name}/imconfig.h",
+		"%{prj.name}/imgui.h",
+		"%{prj.name}/imgui.cpp",
+		"%{prj.name}/imgui_draw.cpp",
+		"%{prj.name}/imgui_internal.h",
+		"%{prj.name}/imgui_tables.cpp",
+		"%{prj.name}/imgui_widgets.cpp",
+		"%{prj.name}/imstb_rectpack.h",
+		"%{prj.name}/imstb_textedit.h",
+		"%{prj.name}/imstb_truetype.h",
+		"%{prj.name}/imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+        filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+		optimize "Off"
+
+	filter "configurations:Optimized"
+		runtime "Release"
+		symbols "Off"
+		optimize "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		symbols "Off"
+		optimize "On"
+
+    filter {}
+		Dependency["imgui"] = {}
+		Dependency["imgui"]["include"] = depsloc .. "imgui/imgui"
+		Dependency["imgui"]["libdir"] = depsloc .. "out/imgui/" .. buildtargetname
