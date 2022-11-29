1dfc278c10fc0b9be57f7351046e5e62b85d896c
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-11-29 12:07:54 +0100

    basic setup

---
+++ src/Calc.cpp
@@ -0,0 +1,6 @@
+#include "Calc.hpp"
+
+int add(int a, int b)
+{
+	return a + b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include "Calc.hpp"
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "1 + 2 = " << add(1, 2) << std::endl;
 	return 0;
 }
