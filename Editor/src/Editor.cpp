bfe47c141a039231e7d5b92f2f8541d32f1c90a4
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-12-02 01:19:22 +0100

    working now

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -6,9 +6,8 @@
 int main(int argc, char** argv)
 {
 	std::cout << "Hello World!" << std::endl;
-	std::cout << "1 + 2 = " << add(1, 2) << std::endl;
-	std::cout << "3 - 2 = " << subtract(3, 2) << std::endl;
+	Init();
 	MakeWindow();
-	std::cout << "Ok..." << std::endl;
+	Terminate();
 	return 0;
 }
