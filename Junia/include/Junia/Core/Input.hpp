a8c6c73e50cdeebba8af374f20a8c4197a52d800
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-12-11 04:49:58 +0100

    Fixed Input polling

--- Junia/include/Junia/Core/Input.hpp
+++ Junia/include/Junia/Core/Input.hpp
@@ -1,19 +1,68 @@
 #pragma once
 #include <utility>
 #include <inttypes.h>
+#include <Junia/Core/Window.hpp>
+#include <Junia/Core/Keycodes.hpp>
 
 namespace Junia
 {
 	class Input
 	{
 	public:
-		static bool IsKeyDown(const int keycode, uint32_t window = 0);
-		static bool IsMouseButtonDown(const int button, uint32_t window = 0);
-		static std::pair<int, int> GetMousePosition(uint32_t window = 0);
+		/**
+		 * @brief Get if a key is pressed
+		 * @param keycode the KeyCode of the key to check for
+		 * @param window the ID of the window to check the key state for
+		 * @return true if the key is pressed, false otherwise
+		*/
+		static bool IsKeyDown(KeyCode keycode, Window::IdType window = 0);
+		/**
+		 * @brief Get if a mouse button is pressed
+		 * @param button the mouse button to check for
+		 * @param window the ID of the window to check the mouse state for
+		 * @return true if the button is pressed, false otherwise
+		*/
+		static bool IsMouseButtonDown(MouseButton button, Window::IdType window = 0);
+		/**
+		 * @brief Get the position of the mouse in a window
+		 * @param window the ID of the window relative to which to get the mouse
+		 *               position
+		 * @return a pair of { x, y } containing the x and y position of the
+		 *           mouse
+		*/
+		static std::pair<int, int> GetMousePosition(Window::IdType window = 0);
 
-		static inline bool IsKeyUp(const int keycode, uint32_t window = 0) { return !IsKeyDown(window, keycode); }
-		static inline bool IsMouseButtonUp(const int button, uint32_t window = 0) { return !IsMouseButtonDown(window, button); }
-		static inline int GetMouseX(uint32_t window = 0) { return GetMousePosition(window).first;  }
-		static inline int GetMouseY(uint32_t window = 0) { return GetMousePosition(window).second; }
+		/**
+		 * @brief Get if a key is not pressed
+		 * @param keycode the KeyCode of the key to check for
+		 * @param window the ID of the window to check the key state for
+		 * @return true if the key is not pressed, false otherwise
+		*/
+		static inline bool IsKeyUp(KeyCode keycode, Window::IdType window = 0)
+			{ return !IsKeyDown(keycode, window); }
+		/**
+		 * @brief Get if a mouse button is not pressed
+		 * @param button the mouse button to check for
+		 * @param window the ID of the window to check the mouse state for
+		 * @return true if the button is not pressed, false otherwise
+		*/
+		static inline bool IsMouseButtonUp(MouseButton button, Window::IdType window = 0)
+			{ return !IsMouseButtonDown(button, window); }
+		/**
+		 * @brief Get the x position of the mouse
+		 * @param window the ID of the window relative to which to get the mouse
+		 *               position
+		 * @return the x position of the mouse
+		*/
+		static inline int GetMouseX(Window::IdType window = 0)
+			{ return GetMousePosition(window).first; }
+		/**
+		 * @brief Get the y position of the mouse
+		 * @param window the ID of the window relative to which to get the mouse
+		 *               position
+		 * @return the y position of the mouse
+		*/
+		static inline int GetMouseY(Window::IdType window = 0)
+			{ return GetMousePosition(window).second; }
 	};
 }
--- Junia/include/Junia/Core/Keycodes.hpp
+++ Junia/include/Junia/Core/Keycodes.hpp
@@ -1,19 +1,24 @@
 #pragma once
-#include <inttypes.h>
 #include <string>
 
 namespace Junia
 {
-	enum class MouseButton : uint8_t
+	/**
+	 * @brief Codes for mouse buttons
+	*/
+	enum class MouseButton : unsigned char
 	{
-		Left = 0,
-		Right = 1,
-		Middle = 2
+		Left = 0,  /** @brief The primary mouse button */
+		Right = 1, /** @brief The secondary mouse button */
+		Middle = 2 /** @brief The middle mouse button */
 	};
 
-	enum class KeyCode : uint8_t
+	/**
+	 * @brief Codes for keys
+	*/
+	enum class KeyCode : unsigned char
 	{
-		HOME           = 2, /* POS1 | HOME */
+		HOME           = 2, /** @brief The POS1 or HOME key */
 		END            = 3,
 		PRINT_SCREEN   = 4,
 		NUM_LOCK       = 5,
@@ -122,8 +127,8 @@ namespace Junia
 		RIGHT_CONTROL  = 131,
 		LEFT_ALT       = 132,
 		RIGHT_ALT      = 133,
-		LEFT_SUPER     = 134, /* left Windows Key / platform equivalent */
-		RIGHT_SUPER    = 135, /* right Windows Key / platform equivalent */
+		LEFT_SUPER     = 134, /** @brief The left Windows Key or platform equivalent */
+		RIGHT_SUPER    = 135, /** @brief The right Windows Key or platform equivalent */
 
 		NUM_ENTER      = 202, /* 0xa | 192 */
 		NUM_MULTIPLY   = 234, /* '*' | 192 */
@@ -144,71 +149,17 @@ namespace Junia
 		NUM_EQUAL      = 253  /* '=' | 192 */
 	};
 
-	static std::string ButtonToString(MouseButton button)
-	{
-		switch (button)
-		{
-		case Junia::MouseButton::Left:   return   "Mouse Left"; break;
-		case Junia::MouseButton::Right:  return  "Mouse Right"; break;
-		case Junia::MouseButton::Middle: return "Mouse Middle"; break;
-		}
-	}
+	/**
+	 * @brief Converts a mouse button code to a string
+	 * @param button the button code to get a string representation for
+	 * @return a human readable string representing the MouseButton
+	*/
+	std::string ButtonToString(MouseButton button);
 
-	static std::string KeyToString(KeyCode keycode)
-	{
-		std::stringstream ss;
-		if (keycode == 39 || (keycode >= 44 && keycode <= 57) || keycode == 59
-			|| keycode == 61 || (keycode >= 65 && keycode <= 93) || keycode == 96) // direct ascii keys
-		{
-			ss << static_cast<char>(keycode);
-		}
-		else if (keycode >= 202) // NUMPAD
-		{
-			ss << "Numpad ";
-			if ((keycode >= 237 && keycode <= 249) || keycode == 234 || keycode == 235 || keycode == 253) ss << static_cast<char>(keycode & ~192);
-			else if (keycode == 202) ss << "Enter";
-			else return "Unknown Key";
-		}
-		else if (keycode >= 101 && keycode <= 125) // F-Keys
-		{
-			ss << "F" << static_cast<int>(keycode - 100);
-		}
-		else
-		{
-			switch (keycode)
-			{
-				case   2: ss <<          "Home"; break;
-				case   3: ss <<           "End"; break;
-				case   4: ss <<  "Print Screen"; break;
-				case   5: ss <<      "Num Lock"; break;
-				case   6: ss <<     "Caps Lock"; break;
-				case   7: ss <<   "Scroll Lock"; break;
-				case   8: ss <<     "Backspace"; break;
-				case   9: ss <<           "Tab"; break;
-				case  10: ss <<         "Enter"; break;
-				case  12: ss <<          "Menu"; break;
-				case  14: ss <<       "Page Up"; break;
-				case  15: ss <<     "Page Down"; break;
-				case  22: ss <<         "Pause"; break;
-				case  26: ss <<        "Insert"; break;
-				case  27: ss <<        "Escape"; break;
-				case  32: ss <<         "Space"; break;
-				case  97: ss <<    "Arrow Left"; break;
-				case  98: ss <<      "Arrow Up"; break;
-				case  99: ss <<   "Arrow Right"; break;
-				case 100: ss <<    "Arrow Down"; break;
-				case 127: ss <<        "Delete"; break;
-				case 128: ss <<    "Left Shift"; break;
-				case 129: ss <<   "Right Shift"; break;
-				case 130: ss <<  "Left Control"; break;
-				case 131: ss << "Right Control"; break;
-				case 132: ss <<      "Left Alt"; break;
-				case 133: ss <<     "Right Alt"; break;
-				case 134: ss <<    "Left Super"; break;
-				case 135: ss <<   "Right Super"; break;
-				default: return "Unknown Key";
-			}
-		}
-		return ss.str();
-	}
+	/**
+	 * @brief Converts a KeyCode to a string
+	 * @param code the key code to get a string representation for
+	 * @return a human readable string representing the KeyCode
+	*/
+	std::string KeyToString(KeyCode code);
 }
