68f3c6723d9cbf6aac89a3e9f01ec7e215127afd
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-12-15 01:29:58 +0100

    Added basis for events

--- Junia/include/Junia/Events/Event.hpp
+++ Junia/include/Junia/Events/Event.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <cinttypes>
+#include <string>
+
+#include <Junia/Core/Keycodes.hpp>
+
 namespace Junia
 {
+	using EventType = uint64_t;
+	using EventCategory = uint64_t;
+
+	#define EVENT_TYPE_CUSTOM 0x0
+
 	class Event
 	{
-	private:
-	protected:
 	public:
-		virtual Event() = default;
+		virtual ~Event() = default;
+
+		virtual EventType GetType() const = 0;
+		virtual EventType GetCategory() const = 0;
+		virtual std::string ToString() const = 0;
+
+	protected:
+		Event() = default;
+	};
+
+	class KeyboardKeyDownEvent : public Event
+	{
+	public:
+		KeyboardKeyDownEvent(KeyCode keyCode) : keyCode(keyCode) { }
+
+		virtual EventType GetType() const { return EVENT_TYPE_CUSTOM; }
+
+	private:
+		KeyCode keyCode;
 	};
 }
