8eea9df6f29a6f6224b177a035397e5cc6002cf4
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2023-04-06 20:59:26 +0200

    [style guide] IdPool.hpp

--- Junia/include/Junia/Core/IdPool.hpp
+++ Junia/include/Junia/Core/IdPool.hpp
@@ -4,80 +4,101 @@
 #include <stack>
 #include <vector>
 
-namespace Junia
-{
+namespace Junia {
+
+constexpr size_t IDPOOL_DEFAULT_RESERVED_FREES = 32;
+
+// -----------------------------------------------------------------------------
+// -------------------------------- Declarations -------------------------------
+// -----------------------------------------------------------------------------
+
+/**
+ * @brief A stack with an accessible container
+ * @tparam T the type of data to store in the stack
+*/
+template<typename T>
+class IdPoolStackAdapter : public std::stack<T, std::vector<T>> {
+public:
 	/**
-	 * @brief A stack with an accessible container
-	 * @tparam T the type of data to store in the stack
+	 * @brief Get the underlying container
+	 * @return a reference to the container the stack has been built on top
+	 *         of
 	*/
-	template<typename T>
-	class IdPoolStackAdapter : public std::stack<T, std::vector<T>>
-	{
-	public:
-		/**
-		 * @brief Get the underlying container
-		 * @return a reference to the container the stack has been built on top
-		 *         of
-		*/
-		std::vector<T>& GetContainer() { return this->c; }
-	};
+	std::vector<T>& GetContainer();
+};
+
+/**
+ * @brief A pool of IDs
+ * @tparam T the numerical data type to use for the IDs
+*/
+template<typename T>
+class IdPool {
+private:
+	IdPoolStackAdapter<T> freeIds{ };
+	T start;
+	T current;
+	T step;
+
+public:
+	/**
+	 * @brief Create a new ID pool
+	 * @param start the minimum value at which the pool starts handing out
+	 *              IDs (defaults to 0)
+	 * @param step the numerical difference between each ID (defaults to 1)
+	 * @param reservedFrees amount of IDs that can be returned to the pool
+	 *                      before a reallocation happens (defaults to 32)
+	*/
+	explicit IdPool(T start = static_cast<T>(0), T step = static_cast<T>(1),
+		size_t reservedFrees = IDPOOL_DEFAULT_RESERVED_FREES);
 
 	/**
-	 * @brief A pool of IDs
-	 * @tparam T the numerical data type to use for the IDs
+	 * @brief Get an unused ID from the pool
+	 * @return a valid ID
 	*/
-	template<typename T>
-	class IdPool
-	{
-	private:
-		IdPoolStackAdapter<T> freeIds{ };
-		T start;
-		T current;
-		T step;
+	T Next();
 
-	public:
-		/**
-		 * @brief Create a new ID pool
-		 * @param start the minimum value at which the pool starts handing out
-		 *              IDs (defaults to 0)
-		 * @param step the numerical difference between each ID (defaults to 1)
-		 * @param reservedFrees amount of IDs that can be returned to the pool
-		 *                      before a reallocation happens (defaults to 32)
-		*/
-		IdPool(T start = static_cast<T>(0), T step = static_cast<T>(1), size_t reservedFrees = 32) :
-			start(start), current(start), step(step)
-		{
-			freeIds.GetContainer().reserve(reservedFrees);
-		}
+	/**
+	 * @brief Return an ID to the pool (unchecked).
+	 * @param id the ID to put back into the pool. In order to maintain
+	 *           uniqueness of IDs only pass IDs that have been returned by
+	 *           the IdPool::Next() function of this IdPool
+	*/
+	void Free(T poolId);
+};
 
-		/**
-		 * @brief Get an unused ID from the pool
-		 * @return a valid ID
-		*/
-		T Next()
-		{
-			T id{ };
-			if (freeIds.empty())
-			{
-				id = current;
-				current = current + step;
-				return id;
-			}
-			id = freeIds.top();
-			freeIds.pop();
-			return id;
-		}
+// -----------------------------------------------------------------------------
+// ------------------------------ Implementations ------------------------------
+// -----------------------------------------------------------------------------
 
-		/**
-		 * @brief Return an ID to the pool (unchecked).
-		 * @param id the ID to put back into the pool. In order to maintain
-		 *           uniqueness of IDs only pass IDs that have been returned by
-		 *           the IdPool::Next() function of this IdPool
-		*/
-		void Free(T id)
-		{
-			if (id == current - step) current = current - step;
-			else freeIds.push(id);
-		}
-	};
+template<typename T>
+inline std::vector<T>& Junia::IdPoolStackAdapter<T>::GetContainer() {
+	return this->c;
 }
+
+template<typename T>
+inline Junia::IdPool<T>::IdPool(T start, T step, size_t reservedFrees)
+	: start(start), current(start), step(step) {
+	freeIds.GetContainer().reserve(reservedFrees);
+}
+
+template<typename T>
+inline T Junia::IdPool<T>::Next() {
+	T poolId{ };
+	if (freeIds.empty()) {
+		poolId = current;
+		current = current + step;
+		return poolId;
+	}
+	poolId = freeIds.top();
+	freeIds.pop();
+	return poolId;
+}
+
+template<typename T>
+inline void IdPool<T>::Free(T poolId) {
+	if (poolId == current - step)
+		current = current - step;
+	else freeIds.push(poolId);
+}
+
+} // namespace Junia
